14e7b4c453afcb2ee3bfb2c542f74c2b88d51883
Aster <aster@vers.site>
2023-10-22 14:08:05 +0800

    Supplementary documentation

--- projects/serde-html/src/traits/mod.rs
+++ projects/serde-html/src/traits/mod.rs
@@ -7,25 +7,56 @@ use crate::values::AttributeValue;
 
 #[allow(unused_variables)]
 pub trait Element: Default + Debug {
+    /// Construct html elements
     fn build(self) -> HtmlElement;
 
+    /// Get the tag of the element, all html elements must have tag
     fn get_tag(&self) -> &str;
 
+    /// Change the tag of the element and return whether the change is successful
     fn set_tag<S>(&mut self, tag: S) -> bool where S: Into<Cow<'static, str>> {
         false
     }
+    /// Get the html attribute based on the given name
     fn get_attribute(&self, name: &str) -> Option<&AttributeValue> {
         None
     }
+    /// Get the mutable html attribute based on the given name
+    fn mut_attribute(&mut self, name: &str) -> Option<&mut AttributeValue> {
+        None
+    }
+    /// According to the given name, set the html attribute and return whether it is successful.
     fn set_attribute<S>(&mut self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
         false
     }
-
-    fn get_class<S>(&self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
+    /// Get the legal html id.
+    ///
+    /// Note that Some will be returned only when the id exists and the value is a string.
+    fn get_id(&self) -> Option<&str> {
+        match self.get_attribute("id")? {
+            AttributeValue::Constant(s) => { Some(s) }
+            AttributeValue::String(s) => { Some(s) }
+            _ => None
+        }
+    }
+    /// Set html id and return whether successful
+    fn set_id<S>(&mut self, id: S) -> bool where S: Into<Cow<'static, str>> {
         false
     }
-
-    fn set_class<S>(&self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
+    /// Get the legal html class.
+    fn get_classes(&self) -> impl Iterator<Item=&str> {
+        [].into_iter()
+    }
+    /// Set html class and return whether successful
+    fn set_classes<S>(&self, classes: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
+        false
+    }
+    /// Add html class and return whether successful
+    fn add_class<S>(&self, class: S) -> bool where S: Into<Cow<'static, str>> {
+        false
+    }
+    /// Remove html class and return whether successful
+    fn remove_class<S>(&self, class: S) -> bool where S: Into<Cow<'static, str>> {
         false
     }
 }
@@ -35,6 +66,7 @@ pub struct HtmlElement {
     tag: Cow<'static, str>,
     classes: Vec<Cow<'static, str>>,
     attributes: IndexMap<Cow<'static, str>, AttributeValue>,
+    children: Vec<HtmlElement>,
 }
 
 impl Default for HtmlElement {
@@ -43,6 +75,7 @@ impl Default for HtmlElement {
             tag: Default::default(),
             classes: vec![],
             attributes: Default::default(),
+            children: vec![],
         }
     }
 }
@@ -56,19 +89,38 @@ impl Element for HtmlElement {
         &self.tag
     }
     fn set_tag<S>(&mut self, tag: S) -> bool where S: Into<Cow<'static, str>> {
-        todo!()
+        self.tag = tag.into();
+        true
     }
     fn get_attribute(&self, name: &str) -> Option<&AttributeValue> {
         self.attributes.get(name)
+    }
+    fn mut_attribute(&mut self, name: &str) -> Option<&mut AttributeValue> {
+        todo!()
     }
     fn set_attribute<S>(&mut self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
         self.attributes.insert(name.into(), value);
         true
     }
-    fn get_class<S>(&self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
-        self.classes.contains(&name.into())
+    fn get_id(&self) -> Option<&str> {
+        todo!()
     }
-    fn set_class<S>(&self, name: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
+    fn set_id<S>(&mut self, id: S) -> bool where S: Into<Cow<'static, str>> {
+        todo!()
+    }
+    fn get_classes(&self) -> impl Iterator<Item=Cow<str>> {
+        self.classes.iter().map(move |s| match s {
+            Cow::Borrowed(c) => { Cow::Borrowed(*c) }
+            Cow::Owned(c) => { Cow::Borrowed(&**c) }
+        })
+    }
+    fn set_classes<S>(&self, classes: S, value: AttributeValue) -> bool where S: Into<Cow<'static, str>> {
+        todo!()
+    }
+    fn add_class<S>(&self, class: S) -> bool where S: Into<Cow<'static, str>> {
+        todo!()
+    }
+    fn remove_class<S>(&self, class: S) -> bool where S: Into<Cow<'static, str>> {
         todo!()
     }
 }
