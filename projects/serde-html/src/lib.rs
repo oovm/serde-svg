e6d72fe98422cb7d728f979a7e8eb43ae1fbff8c
Aster <aster@vers.site>
2023-10-22 14:08:05 +0800

    Supplementary documentation

--- projects/serde-html/src/lib.rs
+++ projects/serde-html/src/lib.rs
@@ -1,11 +1,14 @@
+#![feature(return_position_impl_trait_in_trait)]
 #![deny(missing_debug_implementations, missing_copy_implementations)]
 #![warn(missing_docs, rustdoc::missing_crate_level_docs)]
 #![doc = include_str!("../readme.md")]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
+pub use crate::errors::{Error, Result};
+pub use crate::traits::{Element, HtmlElement};
+
 mod errors;
 mod values;
 mod traits;
 
-pub use crate::errors::{Error, Result};
